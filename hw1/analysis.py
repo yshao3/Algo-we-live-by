5d6a5e8afa3def24337defa06e992ba47ee65798
Yijun Shao <ys849@cornell.edu>
2018-03-04 15:56:35 -0500

    seems finished

--- hw1/analysis.py
+++ hw1/analysis.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Import data
 def importdata(path):
 	res = []
 	read = csv.reader(open(path))
@@ -18,6 +19,7 @@ def importdata(path):
 				res[i].append(int(row[i+1]))
 	return res
 
+# Cross validation
 def Devide(X):
 	Xtrain = []
 	Xtest = []
@@ -26,6 +28,7 @@ def Devide(X):
 		Xtest.append(i[15:])
 	return Xtrain, Xtest
 
+# get k nearest kneighbors according to similarity
 def getkNeighbors(k, Xtrain):
 	similarity = Pearson(Xtrain)
 	kneighbors = []
@@ -36,6 +39,7 @@ def getkNeighbors(k, Xtrain):
 		kneighbors.append(neigh.kneighbors([[1]],return_distance=False).tolist()[0])
 	return kneighbors
 
+# make prediction according to k nearest kneighbors
 def prediction(kneighbors, Xtest):
 	prediction = []
 	for neigh in kneighbors:
@@ -44,33 +48,57 @@ def prediction(kneighbors, Xtest):
 			pred.append(Xtest[neigh[i+1]])
 		prediction.append(np.mean(np.array(np.array(pred)),axis= 0).tolist())
 	return prediction
+
+# calculate mean square error
 def mse_error(pred, real):
 	mse = ((np.array(pred) - np.array(real))**2).mean(axis = 1)
 	return mse
+
+# calculate correlation
 def Pearson(x):
 	return np.corrcoef(x)
+
+# entire knn prediction according to k training set and test set, 
+# and return train and test mean square error
 def knn_prediction(k, Xtrain, Xtest):
 	kneighbors = getkNeighbors(i, Xtrain)
 	train_prediction = prediction(kneighbors, Xtrain)
 	test_prediction = prediction(kneighbors, Xtest)
 	train_mse = mse_error(train_prediction, Xtrain)
 	test_mse =  mse_error(test_prediction, Xtest)
-	return train_mse.mean(axis = 0),test_mse.mean(axis = 0)
-			
+	return train_mse,test_mse
+
+
 X = importdata("mattersOfTaste.csv")	
 Xtrain, Xtest = Devide(X)
+similarity = Pearson(Xtrain)
+# find index of most and least mean similar person
+Index_max = similarity.mean(axis = 1).argmax(axis = 0)
+Index_min = similarity.mean(axis = 1).argmin(axis = 0)
 train_mse = []
 test_mse = []
+max_sim = []
+min_sim = []
+
+# calculate mean square error according to number of nearest neighbors
 for i in range(1, 25):
 	mse_tr, mse_te = knn_prediction(i, Xtrain, Xtest)
-	train_mse.append(mse_tr)
-	test_mse.append(mse_te)
-
+	train_mse.append(mse_tr.mean(axis = 0))
+	test_mse.append(mse_te.mean(axis = 0))
+	max_sim.append(mse_te[Index_max])
+	min_sim.append(mse_te[Index_min])
+print train_mse[0],train_mse[4], train_mse[23]
+print test_mse[0],test_mse[4], test_mse[23]
+print max_sim[0],max_sim[4], max_sim[23]
+print min_sim[0], min_sim[4], min_sim[23]
+# Plot Image
 k = []
 for i in range (1,25):
     k.append(i)
-plta=plt.plot(k,train_mse, '--', label='Train')
-pltb=plt.plot(k,test_mse,'--', label='Test')
+plta=plt.plot(k,train_mse, '-', label='Train')
+pltb=plt.plot(k,test_mse,'-', label='Test')
+pltc=plt.plot(k,max_sim, '-', label='Highest simi person')
+pltd=plt.plot(k,min_sim, '-', label='Lowest simi person')
 plt.ylabel('Mean Square Error')
 plt.xlabel('k nearest neighbor')
 plt.title("Learning Curve Analysis")
